c45a71de52dde4799f6954aa564800290db622ef
klynah <klynah@gmail.com>
2020-04-07 19:58:48 -0400

    Update my.js

--- my.js
+++ my.js
@@ -1,5 +1,5 @@
-$('#sizeslider').on('change', function () {
-    var v = $(this).val();
-    $('div').css('font-size', v + 'rem')
-    $('span').html(v);
+$('input').on('change', function() {
+  var v = $(this).val();
+  $('div').css('font-size', v + 'rem')
+  $('span').html(v);
 });
