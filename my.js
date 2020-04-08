7f967e7ed8abab9d7616d549711c3d118b8c9f5a
klynah <klynah@gmail.com>
2020-04-07 20:27:32 -0400

    Update my.js

--- my.js
+++ my.js
@@ -1,5 +1,7 @@
-$('#slider').on('change', function() {
-  var v = $(this).val();
-  $('div').css('font-size', v + 'rem')
-  $('span').html(v);
+$(document).ready(function() {
+$('#slider').on('change', function () {
+    var v = $(this).val();
+    $('div').css('font-size', v + 'rem')
+    $('span').html(v);
+});
 });
